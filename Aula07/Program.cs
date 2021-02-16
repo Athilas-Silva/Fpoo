17b84c5acd93329b647a52bc29ef337599b9f82a
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 17:27:42 -0300

    Add files via upload

---
+++ Aula07/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula_07___06_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar Variavel
+            double altura, raio, vol;
+
+            //Entrada
+            Console.Write("Qual a altura da Lata: ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Qual o raio da Lata: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+
+            //Processamento
+            vol = 3.1415 * Math.Pow(raio, 2) * altura;
+
+            //Saida
+            Console.Write("O volume da Lata é: " + vol);
+        }
+    }
+}
---
+++ Aula07/aula07.py
@@ -0,0 +1,5 @@
+alt = float(input("Qual a altura da lata: "))
+raio = float(input("Qual o raio da lata: "))
+vol = 3.1415 * pow(raio, 2) * alt
+
+print("o volume da lata é {}".format(2))
