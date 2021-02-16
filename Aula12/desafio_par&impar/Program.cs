3771e86a3107b95217069f8cc2a3ed2a361eba06
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:06:11 -0300

    Add files via upload

---
+++ Aula12/desafio_par&impar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula_12___27_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Varíavel
+            int num;
+
+            //Entrada
+            Console.Write("Digite o número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //Saída
+            if (num % 2 == 0)
+            Console.WriteLine("O número é Par");
+
+            else
+            {
+               Console.WriteLine("O número é Impar");
+            }
+
+        }
+    }
+}
