3083a9249e068a7763fc3c98ee7ab1fb36d0335e
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 14:27:56 -0300

    Add files via upload

---
+++ Aula06_desafio_fah/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula_06___01_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar Variavel
+            float c;
+            float f;
+
+            //entrada(s)
+            Console.Write("Digite a temperatura: ");
+            c = Convert.ToSingle(Console.ReadLine());
+
+            //Processamento
+            f = (9 * c + 160) / 5;
+
+            //Saida
+            Console.Write("Esta temperatura em Fahrenhait é: " + f);
+        }
+    }
+}
---
+++ Aula06_desafio_fah/aula06.py
@@ -0,0 +1,4 @@
+c = float(input("Digite a temperatura em Celcius: "))
+f = (9 * c + 160) / 5
+
+print('A temperatura em Fah é {}'.format(f))
