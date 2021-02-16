bded079f23e953265534adb4f09d529775dd0d68
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 20:21:16 -0300

    Add files via upload

---
+++ Aula25/matriz3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exemplo_matriz_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // matriz 2 x 4 de carros
+            //Ler os elementos da matriz com a estrutura de repetição for()
+            //Variavel
+            string[,] carros = new string[2,4];
+
+            for(int i = 0; i < 2; i++){
+                for(int j = 0; j < 4; j++){
+                    Console.WriteLine("Digite o nome de um carro: ");
+                    carros[i,j] = Console.ReadLine();
+                }
+            }
+
+            //Exibir a matriz com a estrutura de repetição foreach()
+            foreach(string car in carros){
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
---
+++ Aula25/matriz4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exemplo_matriz_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz 4x4
+            //Variavel
+            int [,] numeros = new int[4,4];
+
+            //Entrada da matriz
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    Console.WriteLine($"número [{i},{j}] = ");
+                    numeros[i,j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //Exibindo os elementos diagonal principal
+            for(int i = 0; i < 4; i++){
+                for(int j = 0; j < 4; j++){
+                    if (i == j)
+                    {
+                        Console.WriteLine("Números [{0},{1}] = {2}", i, j, numeros[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
