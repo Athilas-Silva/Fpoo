061ce492c9796cdc4cc3547f5e036aefe4fe7790
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:16:38 -0300

    Add files via upload

---
+++ Aula13/desafio4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Desafio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int n1, n2, n3, n4, mf;
+
+            //Entrada
+            Console.Write("Qual a primeira nota: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Qual a segunda nota: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Qual a terceira nota: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+             Console.Write("Qual a quarta nota: ");
+            n4 = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento
+            mf = (n1 + n2 + n3 + n4) / 4;
+
+            if (mf >= 6)
+            {
+                Console.WriteLine("Parabéns, aluno Aprovado");
+            }
+            else{
+                Console.WriteLine("Infelizmente o aluno está Reprovado");
+            }
+            //Saida
+            Console.WriteLine("A média é: " + mf);
+        }
+    }
+}
---
+++ Aula13/desafio6_switch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Desafio_6___Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            float peso, alt, imc;
+
+            //Entrada
+            Console.Write("Qual é seu peso: ");
+            peso = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Qual sua altura: ");
+            alt = Convert.ToSingle(Console.ReadLine());
+
+            //Processamento
+            imc = peso / (alt * alt);
+
+            switch (imc)
+            {
+                case var _ when imc <= 17:
+                Console.WriteLine("Muito Abaixo do Peso");
+                break;
+                case var _ when imc <= 18.49:
+                Console.WriteLine("Abaixo do Peso");
+                break;
+                case var _ when imc <= 24.99:
+                Console.WriteLine("Peso Normal");
+                break;
+                case var _ when imc <= 29.99:
+                Console.WriteLine("Acima do Peso");
+                break;
+                case var _ when imc <= 34.99:
+                Console.WriteLine("Obesidade I");
+                break;
+                case var _ when imc <= 39.99:
+                Console.WriteLine("Obesidade II");
+                break;
+                case var _ when imc >= 40:
+                Console.WriteLine("Obesidade III");
+                break;
+
+            }
+
+            //Saida
+            Console.Write("Seu IMC é de: " + imc.ToString("0.00"));
+        }
+    }
+}
