e05a16383452d8e038913c3992861137c291ced0
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 20:27:46 -0300

    Add files via upload

---
+++ Aula27/desafio_array_notas/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Desafio_Array___Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Informando as notas, médias e situação do aluno na escola.
+            //Declarando a matriz e o vetor
+            string[] alunos = new string[3];
+            string[] situacao = new string[3];
+            float[,] notas = new float[3,4];
+            float[] soma = new float[3]{0, 0, 0};
+            float[] media = new float[3];
+
+            //Entrada da Array
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("--------------------------------");
+                Console.Write($"Informe o nome do aluno {i+1}º Aluno: ");
+                alunos[i] = Console.ReadLine();
+
+                for(int j = 0; j < 4; j++)
+                {
+                    Console.Write($"Informe a nota {j + 1} do aluno {alunos[i]}: ");
+                    notas[i,j] = float.Parse(Console.ReadLine());
+                    soma[i] = soma[i] + notas[i,j];
+                    media[i] = soma[i] / 4;
+                }
+
+            }
+
+            for(int i = 0; i <media.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------");
+                if (media[i] >= 7)
+                {
+                    Console.WriteLine($"{alunos[i]} esá aprovado \t media = {Math.Round(media[i], 2)}");
+                }
+
+                else if (media[i] < 5)
+                {
+                    Console.WriteLine($"{alunos[i]} está reprovado \t média = {Math.Round(media[i], 2)}");
+                }
+
+                else
+                {
+                    Console.WriteLine($"{alunos[i]} está em recuperação \t media = {Math.Round(media[i], 2)}");
+                }
+            }
+        }
+    }
+}
---
+++ Aula27/desafio_array_v2_notas/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Desafio_Array_v2___Notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variaveis array
+            string[] alunos = new string[5];
+            float[,] notas = new float[5,4];
+            float[] soma = new float[5]{0, 0, 0, 0, 0};
+            float[] media = new float[5];
+
+            //Criando matriz e o vetor
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("-----------------------------------------");
+                Console.Write($"Informe o nome do {i + 1} Aluno: ");
+                alunos[i] = Console.ReadLine();
+
+                    for(int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"Informe a nota {j + 1} do aluno {alunos[i]}: ");
+                        notas[i,j] = float.Parse(Console.ReadLine());
+                        soma[i] = soma[i] + notas[i,j];
+                        media[i] = soma[i] / 4;
+                    }
+
+                    Console.WriteLine("--------------------------------------------------");
+                    Console.WriteLine("Nome \t Situação \t Média \t Nota1 \t Notas2 \t Nota3 \t Nota4 ");
+            }
+
+            for(int i = 0; i < media.Length; i++)
+            {
+                if (media[i] >= 7)
+                {
+                    Console.Write($"{alunos[i]} \t APROVADO \t {Math.Round(media[i], 2)} \t ");
+                    for(int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"{notas[i,j]}\t");
+                    }
+                    Console.WriteLine("\n");
+
+                }
+
+                else if (media[i] < 5)
+                {
+                    Console.Write($"\n{alunos[i]} \t REPROVADO \t {Math.Round(media[i], 2)} \t ");
+                    for(int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"{notas[i,j]}\t");
+                    }
+                    Console.WriteLine("\n");
+                }
+
+                else{
+                    Console.Write($"\n {alunos[i]} \t RECUPERAÇÃO \t {Math.Round(media[i], 2)} \t ");
+                    for(int j = 0; j < 4; j++)
+                    {
+                        Console.Write($"{notas[i,j]}\t");
+                    }
+                    Console.WriteLine("\n");
+                }
+            }
+
+        }
+    }
+}
