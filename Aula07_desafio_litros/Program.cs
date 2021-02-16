84ff176b657b54a408351caf8b5d4c56862a0ef4
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 17:30:05 -0300

    Add files via upload

---
+++ Aula07_desafio_litros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Aula_07___Desafio_em_Litros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar variavel
+            float Tem, Vel, Dis, Lit;
+
+            //entrada
+            Console.Write("Quanto tempo vc gastou: ");
+            Tem = Convert.ToSingle(Console.ReadLine());
+
+            Console.Write("Qual a velocidade: ");
+            Vel = Convert.ToSingle(Console.ReadLine());
+
+            //processamento
+            Dis = Tem * Vel;
+
+            Lit = Dis / 12;
+
+            //saida
+            Console.WriteLine("O tempo gasto na viagem foi de: " + Math.Round(Tem, 2));
+            Console.WriteLine("A Velocidade média foi de: " + Math.Round(Vel, 2));
+            Console.WriteLine("A Distância percorrida foi de: " + Math.Round(Dis, 2));
+            Console.WriteLine("O Consumo em Litros foi de: " + Math.Round(Lit, 2));
+        }
+    }
+}
