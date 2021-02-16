d6057246de6e1877e06a56da54212ddf9a533082
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 19:18:50 -0300

    Add files via upload

---
+++ fatorial_com_for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fatorial_com_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Apresentar o Fatorial de 5
+            //Variavel
+            int fat;
+
+            //Estrutura de Repetição For
+            for (int i = 1; i <= 5; i++)
+            {
+                fat = i * 5;
+                Console.WriteLine("O fatorial de 5 é igual a " + fat);
+            }
+        }
+    }
+}
