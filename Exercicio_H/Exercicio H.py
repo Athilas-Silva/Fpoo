1391bac86c8e9cfdd8614e41b4892e1ef4b317fd
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 17:46:47 -0300

    Delete Exercicio_H directory

--- Exercicio_H/Exercicio H.py
+++
@@ -1,7 +0,0 @@
-alt = float(input("Qual a altura da caixa: "))
-lar = float(input("Qual a largura da caixa: "))
-comp = float(input("Qual o comprimento da caixa: "))
-
-vol = comp * lar * alt
-
-print('O volume da caixa é {}'.format(vol))
