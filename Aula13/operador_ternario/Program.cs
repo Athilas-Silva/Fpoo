52947a60929d2cd409442b36d94daca93da8f664
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:17:19 -0300

    Add files via upload

---
+++ Aula13/operador_ternario/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Operador_Ternario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int n1;
+            string x;
+
+            //Entrada
+            Console.Write("Digite um número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento
+            x = n1 % 2 == 0 ? "Par" : "Impar"; 
+
+            //Saída
+            Console.Write("O número é: " + x);
+        }
+    }
+}
