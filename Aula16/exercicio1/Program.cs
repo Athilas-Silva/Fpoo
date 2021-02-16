cceae82268d3296b9b3e07f2065180fcf7ca838f
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:50:51 -0300

    Add files via upload

---
+++ Aula16/exercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            float x;
+
+            //Entrada
+            Console.Write("Digite o numero: ");
+            x = Convert.ToSingle(Console.ReadLine());
+
+            //Processamento
+
+            if (x > 0)
+            {
+                Console.Write("O número é POSITIVO");
+            }
+            else if (x < 0)
+            {
+                Console.Write("o número é NEGATIVO");
+            }
+            else
+            {
+                Console.Write("O número é NEUTRO");
+            }
+        }
+    }
+}
---
+++ Aula16/exercicio1/positivo&negativo.py
@@ -0,0 +1,11 @@
+x = float(input("Digite um valor: "))
+
+if x > 0:{
+    print("POSITIVO")
+    }
+elif x < 0: {
+    print("NEGATIVO")
+}
+else:{
+    print("NEUTRO")
+}       
