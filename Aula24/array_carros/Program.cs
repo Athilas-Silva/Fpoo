97fb110a991fd5b85782eea9c86091582f8b675e
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 20:07:06 -0300

    Add files via upload

---
+++ Aula24/array_carros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array___Carros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            string[] carros = {"Volvo", "BMW", "Ford", "Mazda"};
+
+            //Processamento
+            for (int i = 0; i < carros.Length; i++)
+            {
+                Console.WriteLine(carros[i]);
+            }
+
+            foreach (string i in carros)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Aula24/array_media_geral/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arrays___Média_Geral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            float [] md = new float[8];
+            float soma = 0;
+
+            //Entrada
+            Console.WriteLine("Obtendo as médias dos alunos \n");
+            for (int i = 0; i < md.Length; i++)
+            {
+                Console.WriteLine("Digite a nota do {0}º aluno: ", i + 1);
+                md[i] = Convert.ToSingle(Console.ReadLine());
+                soma = soma + md[i];
+            }
+            float mg = soma / 8;
+
+            Console.Write("\n A média geral da sala é: " + mg);
+            
+        }
+    }
+}
