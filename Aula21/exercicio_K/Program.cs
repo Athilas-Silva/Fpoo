2d346c5311ccc437bda861cb63ebfc04bf36c840
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 19:49:54 -0300

    Add files via upload

---
+++ Aula21/exercicio_K/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercicio_K
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            double graos;
+
+            graos = Math.Pow(2,63) -1;
+
+            //Saida
+            Console.WriteLine("O resultado é " + graos);
+        }
+    }
+}
