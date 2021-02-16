807c12380eb3870bf381aaecac685400b866a1eb
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 19:18:03 -0300

    Add files via upload

---
+++ Aula19/fatorial2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fatorial_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Apresentar o fatorial de um número dado pelo usuario
+            //Variavel
+            int num, fat, cot;
+
+            //Entrada
+            Console.Write("Informe um valor: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento
+            fat = 1;
+            cot = 1;
+            
+            while (cot <= 5)
+            {
+                fat = num * cot;
+                cot++;
+            }
+            Console.WriteLine("O fatorial é: " + fat);
+        }
+    }
+}
