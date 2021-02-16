7575f6634544255dd71dcbaede7dedfac934f6a3
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 19:11:27 -0300

    Add files via upload

---
+++ Aula19/exemplo1_while/Exemplo While.py
@@ -0,0 +1,7 @@
+import math
+
+i = 15
+
+while i <= 200:
+    print('O quadrado de {} é igual a {}'.format(i, math.pow(i, 2)))
+    i+= 1
---
+++ Aula19/exemplo1_while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exemplo_1___While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Apresentar  os quadrados dos números inteiros de 15 a 200.
+            int i = 0; //Valor inicial
+
+            while (i <= 20) //Valor Final
+            {
+                //Console.WriteLine($"O quadrado de {i} = {Math.Pow(i, 2)}"); //Interpolação
+                Console.WriteLine("O quadrado de " + i + " = " + Math.Pow(i, 2)); //Concatenação
+                i++; //Passo
+            }
+
+        }
+    }
+}
