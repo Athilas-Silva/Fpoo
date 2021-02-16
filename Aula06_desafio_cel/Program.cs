48211a9ea7b07db25a5cbd86a5373ccf268097f3
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 17:22:05 -0300

    Add files via upload

---
+++ Aula06_desafio_cel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula_06___Desafio_Fah
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar Variavél
+            double f;
+            double c;
+
+            //Entrada
+            Console.Write("Qual a temperatura em Fah vc quer converter: ");
+            f = Convert.ToSingle(Console.ReadLine());
+
+            //Processamento
+            c = (f -32) * 5/9;
+            c = Math.Round(c,2);
+
+            //Saida
+            Console.Write("A temperatura em Celcius é: " + c);
+        }
+    }
+}
---
+++ Aula06_desafio_cel/aula06.3.py
@@ -0,0 +1,4 @@
+f = float(input("Digite a temperatura em Fah: "))
+c =  (f -32) * 5/9
+
+print("A temperatura em Celcius é {}".format(c))
