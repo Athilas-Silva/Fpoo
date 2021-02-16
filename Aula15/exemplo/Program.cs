1527fa52a661b434f55672369058a74bd9324f47
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:36:39 -0300

    Add files via upload

---
+++ Aula15/exemplo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            double salario, novsal;
+
+            //Entrada
+            Console.Write("Digite seu Salário: ");
+            salario = Convert.ToSingle(Console.ReadLine());
+
+            //Processamento e Saida
+            if (salario < 500)
+            {
+                novsal = salario * 1.15;
+            }
+            else if (salario <= 1000 && salario <= 500)
+            {
+                novsal = salario * 1.10;
+            }
+            else
+            {
+                novsal = salario * 1.05;
+            }
+            Console.WriteLine("Seu novo salário é: " + novsal.ToString("0.00")); //Concatenação
+            Console.WriteLine($"Seu novo salário é: {novsal.ToString("0.00")}"); //Interpolação
+        }
+    }
+}
