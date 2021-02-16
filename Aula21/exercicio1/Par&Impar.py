0ce50c0249137f630a117b3273e69c3cbfe7ec79
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 19:40:51 -0300

    Add files via upload

---
+++ Aula21/exercicio1/Par&Impar.py
@@ -0,0 +1,9 @@
+num = int(input('Digite um número '))
+
+quociente = num / 2
+res = num - 2 * quociente
+
+if res == num - 2 * 2:
+    print('O número é Par')
+else:
+    print('O número é Impar')    
---
+++ Aula21/exercicio1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Leia um número qualquer e exiba na tela se o mesmo é "Par" ou "Ímpar". NÃO UTILIZAR A FUNÇÃO MOD
+            //PARA A CODIFICAÇÃO DESTE EXERCÍCIO.
+            //Variavel
+            float num, quociente, res;
+
+            //Entrada
+            Console.Write("Informe um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento e Saida
+            quociente = num / 2;
+            res = num - 2 * quociente;
+
+            Console.WriteLine("O resultado é " + quociente);
+
+            if (res == num - 2 * (2))
+            {
+                Console.WriteLine("O número é Par");
+            }
+            else
+            {
+                Console.WriteLine("O número é Impar");
+            }
+        }
+    }
+}
