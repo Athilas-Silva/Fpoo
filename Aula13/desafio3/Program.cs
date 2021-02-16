460ccf48270e3f075df4c9d66fbbf0d557ed6dcb
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 18:13:39 -0300

    Add files via upload

---
+++ Aula13/desafio3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Desafio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int num1, num2, num3, soma, med, maior, menor;
+
+            //Entrada
+            Console.Write("Digite o primeiro valor: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o terceiro valor: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento
+            soma = num1 + num2 + num3;
+
+            med = (num1 + num2 + num3) / 3;
+
+            if (num1 > num2)
+            {
+                maior = num1;
+            }
+            else if (num1 > num3)
+            {
+                maior = num1;
+            }
+            else if (num2 > num3)
+            {
+                maior = num2;
+            }
+            else{
+                maior = num3;
+            }
+            if (num1 < num2)
+            {
+                menor = num1;
+            }
+            else if (num2 < num3)
+            {
+                menor = num2;
+            }
+            else if (num1 < num3)
+            {
+                menor = num1;
+            }
+            else
+            {
+                menor = num3;
+            }
+            //Saida
+            Console.WriteLine("O Maior número digitado foi: " + maior);
+            Console.WriteLine("O Menor número digitado foi: " + menor);
+            Console.WriteLine("A Soma dos números é de: " + soma);
+            Console.WriteLine("A Média dos números é de : " + med);
+
+
+        }
+    }
+}
---
+++ desafio5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Desafio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variavel
+            int n1, n2, n3, n4, mf;
+
+            //Entrada
+            Console.Write("Qual a primeira nota: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Qual a segunda nota: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Qual a terceira nota: ");
+            n3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Qual a quarta nota: ");
+            n4 = Convert.ToInt32(Console.ReadLine());
+
+            //Processamento
+            mf = (n1 + n2 + n3 + n4) / 4;
+
+            if (mf < 5)
+            {
+                Console.WriteLine("O aluno está REPROVADO");
+            }
+            else if (mf >= 7)
+            {
+                Console.WriteLine("Parabéns o Aluno está APROVADO");
+            }
+            else{
+                Console.WriteLine("O aluno está em RECUPERAÇÃO");
+            }
+           
+            //Saida
+            Console.Write("A média do Aluno é: " + mf);
+        }
+    }
+}
