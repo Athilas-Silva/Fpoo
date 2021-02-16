a803f16fd15faca22b4f8bfd6f62faae6b87e8fc
Athilas-Silva <71888055+Athilas-Silva@users.noreply.github.com>
2021-02-16 14:10:10 -0300

    Add files via upload

---
+++ Aula05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula_05___29_09_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar Variavel
+            int num1, num2, soma;
+
+            //Entradas
+            Console.Write("Digite o primeiro numero: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o segundo numero: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            //Processador
+            soma = num1 + num2;
+            
+            //Saida
+            Console.Write("A soma é:" + soma);
+        }
+    }
+}
---
+++ Aula05/aula05.py
@@ -0,0 +1,6 @@
+n1 = int(input("Digite o primeiro valor: "))
+n2 = int(input("Digite o segundo valor: "))
+
+res = n1 + n2
+
+print('A soma é {}'.format(res)) 
